12caf27527d2547f270fc1a496d06bbaacef2533
scrosb2 <scrosb@gmail.com>
2018-07-05 03:31:15 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose.js');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+
 
 
 let app = express();
@@ -30,7 +32,26 @@ app.get('/todos', (req, res) => {
         });
     }, (e) => {
         res.status(400).send(e);
-    })
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    // console.log(req.params.id);
+    //Check if valid
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send(' ');
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send(' ');
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send(' ');
+    });
 });
 
 
